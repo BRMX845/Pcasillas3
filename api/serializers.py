a7038d7667367f786733d7d05caf1808faf2add8
bmrx845 <bquelali8@gmail.com>
2023-04-19 09:55:56 -0400

    permisos y gets,pu,delete,post
    se realizo los cambios en las urls para mas seguridad

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from .models import Usuarios, Casilla, AlquilerCasillas,Departamento
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group
 class DepartamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Departamento
@@ -12,16 +13,26 @@ class UsuariosSerializer(serializers.ModelSerializer):
         return make_password(value)
     class Meta:
         model = Usuarios
-        fields = ['id','username','password','email','celular','ci','departamento','is_superuser','first_name','last_name','is_staff','is_active']
-
+        fields = ['id','username','password','email','celular','ci','departamento','first_name','last_name']
+        #depth=1
+    def create(self, validated_data):
+        validated_data['is_superuser'] = False
+        validated_data['is_staff'] = False
+        validated_data['is_active'] = True
+        user = Usuarios.objects.create_user(**validated_data)
+        group = Group.objects.get(name='cliente') # Cambia el nombre del grupo aquí
+        user.groups.add(group)
+        return user
 class CasillaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Casilla
         fields = '__all__'
-        depth=1 #para mostrar la llaveforanea
+        #depth=1 
+        #para mostrar la llaveforanea
 
 class AlquilerCasillasSerializer(serializers.ModelSerializer):
     class Meta:
         model = AlquilerCasillas
         fields = '__all__'
+        #depth=1
 
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import UsuariosList ,CasillaList,DepartamentoList,AlquilerCasillasList
 urlpatterns = [
-    path('Usuarios/',UsuariosList.as_view()),
+    path('usuarios/',UsuariosList.as_view()),
+    path('usuarios/<int:pk>/',UsuariosList.as_view(),name='Usuarios-detail'),
     path('departamentos/',DepartamentoList.as_view()),
+    path('departamentos/<int:pk>/',DepartamentoList.as_view(),name='departamento-detail'),
     path('casillas/',CasillaList.as_view()),
-    path('alquilercasillas/',AlquilerCasillasList.as_view())
+    path('casillas/<int:pk>/',CasillaList.as_view(),name='casillas-detail'),
+    path('alquilercasillas/',AlquilerCasillasList.as_view()),
+    path('alquilercasillas/<int:pk>/',AlquilerCasillasList.as_view(),name='alquiler-detail'),
 ]
